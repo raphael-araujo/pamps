009d72a15379da165e4da77a6bc60654fa58b905
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2025-02-05 17:11:16 -0300

    Adicionando Models de postagem

--- pamps/models/user.py
+++ pamps/models/user.py
@@ -1,11 +1,13 @@
-from typing import Optional
+from typing import Optional, List
 
 from pydantic import BaseModel
-# from pydantic. import BaseModel
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 
 from pamps.security import HashedPassword
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from pamps.models.post import Post
 
 class User(SQLModel, table=True):
     """Represents the User Model"""
@@ -16,6 +18,9 @@ class User(SQLModel, table=True):
     avatar: Optional[str] = None
     bio: Optional[str] = None
     password: HashedPassword
+
+    # it populates the .user attribute on the Content Model
+    posts: List["Post"] = Relationship(back_populates="user")
 
 
 class UserResponse(BaseModel):
