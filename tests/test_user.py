55ff13ff71a0785daf008ebab34793518911b179
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2025-05-09 11:44:11 -0300

    Adicionado testes para o endpoint de seguir usuário

---
+++ tests/test_user.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+@pytest.mark.order(3)
+def test_follow_user(api_client_user1, api_client_user2):
+    """User2 will follow user1"""
+
+    api_client_user1.get(
+        "/user/",
+    )
+    response = api_client_user2.post(
+        "user/follow/1",
+    )
+    assert response.status_code == 201
+
+
+@pytest.mark.order(3)
+def test_follow_yourself_should_return_status_403(api_client_user1, api_client_user2):
+    """User1 will try follow yourself."""
+
+    response = api_client_user1.post(
+        "user/follow/1",
+    )
+
+    assert response.status_code == 403
+    assert "You cannot follow yourself" in response.json()["detail"]
